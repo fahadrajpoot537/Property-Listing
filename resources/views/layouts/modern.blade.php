46895f72056d44cb38816733ff05e3e769a8274e
Fahad Bin Khalid <fahadrajpoot537@gmail.com>
2026-02-23 14:38:31 +0500

    Favicon

--- resources/views/layouts/modern.blade.php
+++ resources/views/layouts/modern.blade.php
@@ -7,6 +7,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>@yield('title', 'PropertyFinda - Premium Property Listings')</title>
+    <link rel="icon" type="image/png" href="{{ asset('logoor.png') }}">
 
     <!-- Google Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
